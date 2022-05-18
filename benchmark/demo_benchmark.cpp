323967969e86e229a751946642055e5f8e94f57a
gaBUBUNts <89221310+gaBUBUNts@users.noreply.github.com>
2022-05-18 21:18:14 +0300

    Реализовал introsort. Потом пропишу нормальный hpp и пропишу heap sort не через std.

--- benchmark/demo_benchmark.cpp
+++ benchmark/demo_benchmark.cpp
@@ -6,7 +6,7 @@
 #include <sstream>      // stringstream
 
 // подключаем вашу структуру данных
-#include "data_structure.hpp"
+#include "introsort.hpp"
 
 using namespace std;
 using namespace itis;
